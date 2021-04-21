8637ad6adc39a6686325a0dc11ae36353c708a24
livia.aurich <livia.aurich@basis.com.br>
2021-04-21 15:48:59 -0300

    Configuraçao inicial do projeto.

---
+++ src/test/java/com/liviaaurich/carteiraservice/CarteiraUsuarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liviaaurich.carteiraservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraUsuarioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
